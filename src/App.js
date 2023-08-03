aa9f6b1f278a8df70637440ddf198bc67ee0036b
Christian Anderson <tehninjawaffle@gmail.com>
2023-08-03 15:22:22 -0700

    sequestered my data to another file and imported it. Added a change and submit handler as well as added them to my inputs and form. Also shifted the position of form. Made modifications to the boilerplate to remove an error.Now, to move all of the to form.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from "react"
-
-const teamMemberList = [
- {name: "Jake", job: "technician", email: "JakeRul3s@hotmail.com"},
- {name: "Lissandra", job: "Ruler of the Freljord", email: "2Cool4U@gmail.com"},
- {name: "Firant", job: "Pheonix", email: "paym33nough@gmail.com"}
-]
-
+import React, { useState } from "react"
+import teamMemberList from './components/DummyData';
 
 function App() {
   const [teamMember, setTeamMember] = useState(teamMemberList)
-  console.log(teamMember)
+  const [formValues, setFormValues] = useState({ name: "", job: "", email: ""})
+
+  const change = (evt) => {
+    const { name, value} = evt.target;
+    setFormValues({ ...formValues, [name]: value})
+  }
+  const submit = (evt) => {
+    evt.preventDefault();
+    setTeamMember(teamMember.concat({ name: formValues.name, job: formValues.job, email: formValues.email}))
+  }
+
+  // console.log(teamMember)
   return (
-    <form>
+    
       <div className="App">
         <h1>Our valuable team</h1>
         {teamMember.map((member, idx) => {
@@ -21,38 +26,45 @@ function App() {
             <div className="wrapper">
               <h2 key={idx} className="nameAndJob">
                 {member.name} is a {member.job}
+              </h2>
+              <div className="wrapper2">
                 <h4 className="email">Need their help? Reach them here: {member.email} </h4>
-             </h2>
+              </div> 
             </div>
           )
         })}
 
-
+      <form onSubmit={submit}>
         <h3>Not here? Add yourself!</h3>
           <input
           placeholder="Your name"
           name="name"
           type="text"
-          value=""
-
+          value={formValues.name}
+          onChange={change}
           />
           <input 
           placeholder="Your occupation"
           name="job"
           type="text"
-          value=""
-          
+          value={formValues.job}
+          onChange={change}
           />
           <input
           placeholder="Your email"
           name="email"
           type="email"
-          value=""
-
+          value={formValues.email}
+          onChange={change}
           />
-
+          <input
+          onChange={change}
+          type="submit"
+          value="Create Yourself"
+          />
+      
+      </form>
       </div>
-    </form>
   );
 }
 
---
+++ src/components/DummyData.js
@@ -0,0 +1,8 @@
+
+const teamMemberList = [
+  {name: "Jake", job: "technician", email: "JakeRul3s@hotmail.com"},
+  {name: "Lissandra", job: "Ruler of the Freljord", email: "2Cool4U@gmail.com"},
+  {name: "Firant", job: "Pheonix", email: "paym33nough@gmail.com"}
+ ]
+
+ export default teamMemberList
